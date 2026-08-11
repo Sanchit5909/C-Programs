5b713539eeff4c430bf620daf3c0bc740b2f0e96
Sanchit Thanekar <sanchitthanekar5909@gmail.com>
2026-08-11 04:38:33 +0000

    Commit

---
+++ 26ComplexAdd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int r1, i1, r2, i2;
+
+    cout << "Enter First Complex Number: ";
+    cin >> r1 >> i1;
+
+    cout << "Enter Second Complex Number: ";
+    cin >> r2 >> i2;
+
+    cout << "Sum = " << r1 + r2 << " + " << i1 + i2 << "i";
+
+    return 0;
+}
